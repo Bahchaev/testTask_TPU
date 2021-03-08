664e49f80b1bb4a830b401c684088fc9816df235
Bahchaev <wbahas@mail.ru>
2021-03-08 22:07:50 +0700

    some code clear

--- src/Components/NavBar/index.js
+++ src/Components/NavBar/index.js
@@ -7,17 +7,17 @@ import logo from '../../Images/logo-tpu.png'
 function NavBar() {
     return (
         <Navbar collapseOnSelect expand="md" className={styles.navBar} variant="dark">
-                <Navbar.Brand href=""><img src={logo} alt="logo"/></Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className={styles.nav}>
-                        <Nav.Link href="" className={styles.link}>О компании</Nav.Link>
-                        <Nav.Link href="" className={styles.link}>Научтные проекты</Nav.Link>
-                        <Nav.Link href="" className={styles.link}>Поддержать</Nav.Link>
-                        <Nav.Link href="" className={styles.link}>Проекты ТПУ</Nav.Link>
-                        <Nav.Link href="" className={styles.link}>Меценаты</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
+            <Navbar.Brand href=""><img src={logo} alt="logo"/></Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className={styles.nav}>
+                    <Nav.Link href="" className={styles.link}>О компании</Nav.Link>
+                    <Nav.Link href="" className={styles.link}>Научтные проекты</Nav.Link>
+                    <Nav.Link href="" className={styles.link}>Поддержать</Nav.Link>
+                    <Nav.Link href="" className={styles.link}>Проекты ТПУ</Nav.Link>
+                    <Nav.Link href="" className={styles.link}>Меценаты</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
         </Navbar>
     )
 }
